38b9228ea11598ef61c2280f16b96ec98ab2cf32
Adrian Osborne <adrian.b.osborne@gmail.com>
2024-08-01 01:07:13 +1000

    DONE!

--- files/main.py
+++ files/main.py
@@ -2,15 +2,47 @@ import os
 import random
 import shutil
 import json
+import threading
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton,
     QFileDialog, QLabel, QTreeWidget, QTreeWidgetItem, QLineEdit,
-    QWidget, QTableWidget, QTableWidgetItem, QCheckBox, QDialog, QInputDialog, QMessageBox
+    QWidget, QTableWidget, QTableWidgetItem, QCheckBox, QDialog, QInputDialog, QMessageBox, QSplashScreen
 )
-from PyQt5.QtCore import Qt
-
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal, QThread
+from PyQt5.QtGui import QPixmap
 
 CONFIG_FILE = "files/config.json"
+
+class LoadingScreen(QSplashScreen):
+    def __init__(self, message="Loading..."):
+        super().__init__(QPixmap(400, 300))
+        self.showMessage(message, Qt.AlignCenter | Qt.AlignBottom, Qt.white)
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+
+class WorkerThread(QThread):
+    update_toggle_signal = pyqtSignal(str, bool)
+    finished_signal = pyqtSignal()
+
+    def __init__(self, files, enable, batch=False):
+        super().__init__()
+        self.files = files
+        self.enable = enable
+        self.batch = batch
+
+    def run(self):
+        for file in self.files:
+            print(f"Toggling file: {file}")
+            self.toggle_file(file, self.enable)
+            if not self.batch:
+                self.update_toggle_signal.emit(file, self.enable)
+        self.finished_signal.emit()
+
+    def toggle_file(self, file, enable):
+        if enable:
+            new_file = file.replace(".disabled", ".psarc")
+        else:
+            new_file = file.replace(".psarc", ".disabled")
+        os.rename(file, new_file)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -37,22 +69,22 @@ class MainWindow(QMainWindow):
         # DLC management buttons
         self.enable_all_button = QPushButton("Enable All DLC")
         self.enable_all_button.setEnabled(False)
-        self.enable_all_button.clicked.connect(self.enable_all_dlc)
+        self.enable_all_button.clicked.connect(self.handle_enable_all)
         layout.addWidget(self.enable_all_button)
 
         self.disable_all_button = QPushButton("Disable All DLC")
         self.disable_all_button.setEnabled(False)
-        self.disable_all_button.clicked.connect(self.disable_all_dlc)
+        self.disable_all_button.clicked.connect(self.handle_disable_all)
         layout.addWidget(self.disable_all_button)
 
         self.enable_random_button = QPushButton("Enable Random DLC")
         self.enable_random_button.setEnabled(False)
-        self.enable_random_button.clicked.connect(self.enable_random_dlc)
+        self.enable_random_button.clicked.connect(self.handle_enable_random)
         layout.addWidget(self.enable_random_button)
 
         self.reset_data_button = QPushButton("Reset All Data")
         self.reset_data_button.setEnabled(False)
-        self.reset_data_button.clicked.connect(self.reset_all_data)
+        self.reset_data_button.clicked.connect(self.handle_reset_all_data)
         layout.addWidget(self.reset_data_button)
 
         # DLC directory tree
@@ -98,12 +130,14 @@ class MainWindow(QMainWindow):
             self.directory_tree.setEnabled(True)
 
     def load_directory_tree(self):
+        self.show_loading_screen()
         self.directory_tree.clear()
         if self.dlc_folder:
             root_item = QTreeWidgetItem([os.path.basename(self.dlc_folder)])
             root_item.setData(0, Qt.UserRole, self.dlc_folder)
             self.directory_tree.addTopLevelItem(root_item)
             self.add_subdirectories(root_item, self.dlc_folder)
+        self.loading_screen.close()
 
     def add_subdirectories(self, tree_item, path):
         for item in os.listdir(path):
@@ -116,48 +150,36 @@ class MainWindow(QMainWindow):
 
     def filter_tree(self):
         search_text = self.search_bar.text().lower()
-        for i in range(self.directory_tree.topLevelItemCount()):
-            item = self.directory_tree.topLevelItem(i)
-            self.filter_tree_item(item, search_text)
+        self.filter_tree_item(self.directory_tree.invisibleRootItem(), search_text)
 
     def filter_tree_item(self, item, search_text):
         match = search_text in item.text(0).lower()
-        item.setHidden(not match)
         for i in range(item.childCount()):
             child = item.child(i)
             child_match = self.filter_tree_item(child, search_text)
             match = match or child_match
+        item.setHidden(not match)
         return match
 
-    def enable_all_dlc(self):
-        self.toggle_dlc_files(enable=True)
+    def handle_enable_all(self):
+        files = self.get_all_dlc_files()
+        self.start_worker_thread(files, enable=True, batch=True)
 
-    def disable_all_dlc(self):
-        self.toggle_dlc_files(enable=False)
+    def handle_disable_all(self):
+        files = self.get_all_dlc_files()
+        self.start_worker_thread(files, enable=False, batch=True)
 
-    def enable_random_dlc(self):
+    def handle_enable_random(self):
         x, ok = QInputDialog.getInt(self, "Enable Random DLC", "Enter the number of DLCs to enable:")
         if ok:
             all_files = self.get_all_dlc_files()
             random_files = random.sample(all_files, min(x, len(all_files)))
-            for file in random_files:
-                self.toggle_file(file, enable=True)
+            self.start_worker_thread(random_files, enable=True, batch=True)
 
-    def toggle_dlc_files(self, enable):
-        for root, dirs, files in os.walk(self.dlc_folder):
-            for file in files:
-                if file.endswith(".psarc") or file.endswith(".disabled"):
-                    self.toggle_file(os.path.join(root, file), enable)
-
-    def toggle_file(self, file, enable):
-        if enable:
-            new_file = file.replace(".disabled", ".psarc")
-        else:
-            new_file = file.replace(".psarc", ".disabled")
-        os.rename(file, new_file)
-
-    def reset_all_data(self):
-        self.toggle_dlc_files(enable=True)
+    def handle_reset_all_data(self):
+        self.show_loading_screen()
+        files = self.get_all_dlc_files()
+        self.start_worker_thread(files, enable=True, batch=True)
         self.dlc_folder = None
         self.dlc_folder_label.setText("No DLC folder selected")
         self.directory_tree.clear()
@@ -167,7 +189,9 @@ class MainWindow(QMainWindow):
         self.reset_data_button.setEnabled(False)
         self.search_bar.setEnabled(False)
         self.directory_tree.setEnabled(False)
-        self.save_config()  # Clear the saved DLC folder
+        if os.path.exists(CONFIG_FILE):
+            os.remove(CONFIG_FILE)
+        self.loading_screen.close()
 
     def edit_directory(self, item, column):
         directory_path = item.data(0, Qt.UserRole)
@@ -187,8 +211,10 @@ class MainWindow(QMainWindow):
             self.show_directory_editor(directories)
 
     def show_directory_editor(self, directories):
-        editor_window = DirectoryEditorWindow(directories)
-        editor_window.exec_()
+        self.hide()
+        self.editor_window = DirectoryEditorWindow(directories, self)
+        self.editor_window.finished_signal.connect(self.show)
+        self.editor_window.show()
 
     def get_all_dlc_files(self):
         dlc_files = []
@@ -209,15 +235,41 @@ class MainWindow(QMainWindow):
         with open(CONFIG_FILE, "w") as f:
             json.dump(config, f)
 
+    def start_worker_thread(self, files, enable, batch=False):
+        self.show_loading_screen()
+        self.worker_thread = WorkerThread(files, enable, batch)
+        self.worker_thread.update_toggle_signal.connect(self.update_toggle_box)
+        self.worker_thread.finished_signal.connect(self.worker_thread_finished)
+        self.worker_thread.start()
+
+    def update_toggle_box(self, file, enabled):
+        # Implement logic to update the toggle box associated with the file
+        print(f"Updating toggle box for {file} to {'enabled' if enabled else 'disabled'}")
+
+    def worker_thread_finished(self):
+        self.loading_screen.close()
+        print("Worker thread finished")
+
+    def show_loading_screen(self):
+        self.loading_screen = LoadingScreen()
+        self.loading_screen.show()
 
 class DirectoryEditorWindow(QDialog):
-    def __init__(self, directories):
-        super().__init__()
+    finished_signal = pyqtSignal()
+
+    def __init__(self, directories, parent=None):
+        super().__init__(parent)
         self.setWindowTitle("Edit Directory")
         self.setGeometry(100, 100, 800, 600)  # Changed window size to span entire length
         self.directories = directories
 
         layout = QVBoxLayout()
+
+        # Search bar for files
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search files...")
+        self.search_bar.textChanged.connect(self.filter_files)
+        layout.addWidget(self.search_bar)
 
         # Table of DLC files
         self.table = QTableWidget()
@@ -231,16 +283,20 @@ class DirectoryEditorWindow(QDialog):
         button_layout = QHBoxLayout()
 
         enable_all_button = QPushButton("Enable All")
-        enable_all_button.clicked.connect(self.enable_all)
+        enable_all_button.clicked.connect(self.handle_enable_all)
         button_layout.addWidget(enable_all_button)
 
         disable_all_button = QPushButton("Disable All")
-        disable_all_button.clicked.connect(self.disable_all)
+        disable_all_button.clicked.connect(self.handle_disable_all)
         button_layout.addWidget(disable_all_button)
 
         enable_random_button = QPushButton("Enable Random")
-        enable_random_button.clicked.connect(self.enable_random)
+        enable_random_button.clicked.connect(self.handle_enable_random)
         button_layout.addWidget(enable_random_button)
+
+        back_button = QPushButton("Back")
+        back_button.clicked.connect(self.close)
+        button_layout.addWidget(back_button)
 
         layout.addLayout(button_layout)
 
@@ -272,30 +328,60 @@ class DirectoryEditorWindow(QDialog):
         old_file_path = os.path.join(directory, file)
         if enable:
             new_file_path = old_file_path.replace(".disabled", ".psarc")
-            print(file + " has been enabled")
         else:
             new_file_path = old_file_path.replace(".psarc", ".disabled")
-            print(file + " has been disabled")
         os.rename(old_file_path, new_file_path)
+        print(f"Toggled file {old_file_path} to {new_file_path}")
         self.load_files()
 
-    def enable_all(self):
-        for i in range(self.table.rowCount()):
-            checkbox = self.table.cellWidget(i, 1)
-            checkbox.setChecked(True)
+    def handle_enable_all(self):
+        files = self.get_all_files()
+        self.start_worker_thread(files, enable=True, batch=False)
 
-    def disable_all(self):
-        for i in range(self.table.rowCount()):
-            checkbox = self.table.cellWidget(i, 1)
-            checkbox.setChecked(False)
+    def handle_disable_all(self):
+        files = self.get_all_files()
+        self.start_worker_thread(files, enable=False, batch=False)
 
-    def enable_random(self):
+    def handle_enable_random(self):
         x, ok = QInputDialog.getInt(self, "Enable Random Files", "Enter the number of files to enable:")
         if ok:
-            checkboxes = [self.table.cellWidget(i, 1) for i in range(self.table.rowCount())]
-            random_checkboxes = random.sample(checkboxes, min(x, len(checkboxes)))
-            for checkbox in random_checkboxes:
-                checkbox.setChecked(True)
+            all_files = self.get_all_files()
+            random_files = random.sample(all_files, min(x, len(all_files)))
+            self.start_worker_thread(random_files, enable=True, batch=False)
+
+    def get_all_files(self):
+        files = []
+        for directory in self.directories:
+            for file in os.listdir(directory):
+                if file.endswith(".psarc") or file.endswith(".disabled"):
+                    files.append(os.path.join(directory, file))
+        return files
+
+    def start_worker_thread(self, files, enable, batch=False):
+        self.worker_thread = WorkerThread(files, enable, batch)
+        self.worker_thread.update_toggle_signal.connect(self.update_toggle_box)
+        self.worker_thread.finished_signal.connect(self.worker_thread_finished)
+        self.worker_thread.start()
+
+    def update_toggle_box(self, file, enabled):
+        # Implement logic to update the toggle box associated with the file
+        print(f"Updating toggle box for {file} to {'enabled' if enabled else 'disabled'}")
+
+    def worker_thread_finished(self):
+        print("Worker thread finished")
+        self.load_files()
+
+    def filter_files(self):
+        search_text = self.search_bar.text().lower()
+        for row in range(self.table.rowCount()):
+            file_item = self.table.item(row, 0)
+            file_name = file_item.text().lower()
+            match = search_text in file_name
+            self.table.setRowHidden(row, not match)
+
+    def closeEvent(self, event):
+        self.finished_signal.emit()
+        super().closeEvent(event)
 
 
 if __name__ == "__main__":
